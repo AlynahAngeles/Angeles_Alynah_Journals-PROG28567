081478374eec438b46feaea9452dd3ec96c72d53
AlynahAngeles <angeleju@sheridancollege.ca>
2025-09-14 01:55:21 -0400

    saving work, didn't finish task 2b

--- Assets/Scripts/RowGeneration.cs
+++ Assets/Scripts/RowGeneration.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RowGeneration : MonoBehaviour
 {
-    public float SquareNumberInput;
+
+    public InputField SquareNumberInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

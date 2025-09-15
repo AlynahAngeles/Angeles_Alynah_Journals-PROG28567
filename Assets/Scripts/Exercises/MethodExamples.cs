9bdc44d609b92aec1364eb6e4d391a81e79ac1a2
AlynahAngeles <150286316+AlynahAngeles@users.noreply.github.com>
2025-09-15 14:55:29 -0400

    Finished method examples demo

--- Assets/Scripts/Exercises/MethodExamples.cs
+++ Assets/Scripts/Exercises/MethodExamples.cs
@@ -11,8 +11,24 @@ public class MethodExamples : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        DrawBoxAtPosition(mousePosition, Vector2.one, new Color(1f, 1f, 1f, 0.5f));
     }
 
-    private void DrawBoxAtPosition(Vector2 position, )
+    private void DrawBoxAtPosition(Vector2 position, Vector2 size, Color colour)
+    {
+        float halfWidth = size.x / 2f;
+        float halfHeight = size.y / 2f;
+
+        Vector2 topLeft = position + new Vector2(-halfWidth, halfHeight);
+        Vector2 topRight = topLeft + new Vector2(size.x, 0);
+        Vector2 bottomRight = topRight + new Vector2(0, -size.y);
+        Vector2 bottomLeft = bottomRight + new Vector2(-size.x, 0);
+
+        Debug.DrawLine(topLeft, topRight, colour, 0.5f);
+        Debug.DrawLine(topRight, bottomRight, colour, 0.5f);
+        Debug.DrawLine(bottomRight, bottomLeft, colour, 0.5f);
+        Debug.DrawLine(bottomLeft, topLeft, colour, 0.5f);
+
+    }
 }

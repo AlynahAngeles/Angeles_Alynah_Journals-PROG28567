a8198af418f2466dcbf3519a2c81a448d3bae2a7
AlynahAngeles <angeleju@sheridancollege.ca>
2025-09-21 22:24:14 -0400

    Implemented task 1 and 2 for journal 2. This includes spawning in a row of bombs by pressing B and randomly spawning a bomb in a corner using C.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -3,23 +3,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Transform enemyTransform;
+    public Vector2 bombOffset;
     public GameObject bombPrefab;
+    public int numberOfBombs;
+    public float bombSpacing = -2f;
+    public Transform enemyTransform;
     public List<Transform> asteroidTransforms;
+    private float inDistance = 2f;
     
     // Update is called once per frame
     void Update()
     {
+        SpawnBombAtOffset();
+        cornerBombs();
+    }
+
+    private void SpawnBombAtOffset()
+    {
         if (Input.GetKeyDown(KeyCode.B))
         {
-            SpawnBombAtOffset(new Vector3(0, 1));
-            //or SpawnBombAtOffset(new Vector3.up);
+            for(int i = 0; i < numberOfBombs; i++)
+            {
+                Vector3 spawnPosition = transform.position + -(transform.up * (bombSpacing * i)) + (transform.right * bombOffset.x);
+                Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+            }
         }
     }
 
-    private void SpawnBombAtOffset(Vector3 inOffset)
+    private void cornerBombs()
     {
-        Vector3 spawnPos = transform.position + inOffset;
-        Instantiate(bombPrefab, spawnPos, Quaternion.identity);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Vector3[] corners = new Vector3[]
+            {
+                new Vector3(1,1,0).normalized,
+                new Vector3(-1,1,0).normalized,
+                new Vector3(1,-1,0).normalized,
+                new Vector3(-1,-1,0).normalized,
+            };
+
+            int randomIndex = Random.Range(0, corners.Length);
+            Vector3 cornerDirection = corners[randomIndex];
+
+            Vector3 spawnPosition = transform.position + cornerDirection * inDistance;
+
+            Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
+

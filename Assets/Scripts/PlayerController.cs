b7f6c9486c4cc57054a59061ad5d4263f117a86f
AlynahAngeles <150286316+AlynahAngeles@users.noreply.github.com>
2025-11-17 19:56:03 -0500

    saving before doing task 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D player;
+    public float playerSpeed = 7f;
+
     public enum FacingDirection
     {
         left, right
@@ -10,20 +13,36 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
-        MovementUpdate(playerInput);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerInput.x = -1;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerInput.x = 1;
+        }
+
+
+            MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+
+        player.linearVelocity = new Vector2(playerInput.x * playerSpeed, player.linearVelocity.y);
+        player.linearDamping = 0f;
 
     }
 
